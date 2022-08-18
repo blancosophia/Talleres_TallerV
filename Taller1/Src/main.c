9e690c841f0b0b86f4d965241583cd348dcf2bd3
blancosophia <sblancor@unal.edu.co>
2022-08-18 18:57:10 -0500

    Entrega Tarea 1

--- Taller1/Src/main.c
+++ Taller1/Src/main.c
@@ -4,14 +4,17 @@
  * @author         : blancosophia
  * @brief          : Main program body
  ******************************************************************************
-
  */
 
 #include <stdint.h>
 
+unsigned int anos = 0;
+signed short dias = 0;
 
 int main(void)
 {
+	anos = 10;
+	dias = 365;
     /* Loop forever */
 	while(1){
 
